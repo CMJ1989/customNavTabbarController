785814dd59ef1606d511b77b46cfc72af0ed405d
cmj <chimj@zgxcw.com>
2016-06-28 17:30:51 +0800

    add a project

---
+++ CMJNavTabbarController/CMJNavTabbarController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CMJNavTabbarController
+//
+//  Created by ChiMJ on 2016/6/28.
+//  Copyright © 2016年 zgtx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CMJNavTabbarController/CMJNavTabbarController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CMJNavTabbarController
+//
+//  Created by ChiMJ on 2016/6/28.
+//  Copyright © 2016年 zgtx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
